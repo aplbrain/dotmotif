cb0bea13c8e49ec9bb4afd26dbaf3824d13e0014
Jordan Matelsky <j6k4m8@gmail.com>
2018-10-24 13:54:54 -0400

    Add ingester

--- dotmotif/ingest/__init__.py
+++ dotmotif/ingest/__init__.py
@@ -1,4 +1,5 @@
 # Standard installs:
+import json
 import os
 
 # Non-standard installs:
@@ -44,6 +45,49 @@ class PrincetonIngester(Ingester):
             headerfile.write(":START_ID(Neuron),:END_ID(Neuron)")
 
         edge_fnames = export_df.to_csv(
+            self.export_dir + "export-synapses-*.csv", index=False, header=False
+        )
+
+        return node_fnames + [headerpath] + edge_fnames
+
+
+class HarvardIngester(Ingester):
+
+    def __init__(self, json_path: str, export_dir: str) -> None:
+        self.json_path = json_path
+        self.export_dir = export_dir.rstrip("/") + "/"
+        if not os.path.isdir(self.export_dir):
+            os.makedirs(self.export_dir)
+
+    def ingest(self) -> List[str]:
+        data = json.load(open(self.json_path, 'r'))
+        export_df = pd.DataFrame({
+            ":START_ID(Neuron)": data["neuron_1"],
+            ":END_ID(Neuron)":   data["neuron_2"]
+        })
+
+        node_names = (
+            export_df[":START_ID(Neuron)"].append(export_df[":END_ID(Neuron)"])
+        ).unique()
+
+        node_names_dd = dd.from_pandas(pd.DataFrame({
+            "neuronId:ID(Neuron)": node_names
+        }), npartitions=1)
+
+        node_fnames = node_names_dd.to_csv(
+            self.export_dir + "export-neurons-*.csv",
+            index=False, header=["neuronId:ID(Neuron)"]
+        )
+
+        # This is absurd, but neo4j can't tolerate file headers in every CSV,
+        # and dask can't NOT.
+        # So We print off a header file first.
+        export_df_dd = dd.from_pandas(export_df, npartitions=1)
+        headerpath = self.export_dir + "export-synapses-00.csv"
+        with open(headerpath, 'w') as headerfile:
+            headerfile.write(":START_ID(Neuron),:END_ID(Neuron)")
+
+        edge_fnames = export_df_dd.to_csv(
             self.export_dir + "export-synapses-*.csv", index=False, header=False
         )
 
