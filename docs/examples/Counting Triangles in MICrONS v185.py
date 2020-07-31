596ef9879b51bc33a660481105b00db6bcadb869
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2020-07-31 12:29:39 -0400

    Create Counting Triangles in MICrONS v185.py

---
+++ docs/examples/Counting Triangles in MICrONS v185.py
@@ -0,0 +1,20 @@
+from dotmotif import dotmotif, NetworkXExecutor
+from dotmotif.ingest import CSVEdgelistConverter
+
+
+graph = CSVEdgelistConverter(
+    "https://zenodo.org/record/3710459/files/soma_subgraph_synapses_spines_v185.csv?download=1",
+    "pre_root_id", "post_root_id"
+).to_graph()
+
+E = NetworkXExecutor(graph=graph)
+
+motif = dotmotif().from_motif("""
+A -> B
+B -> C
+C -> A
+""")
+
+results = E.find(motif)
+
+print(len(results))
