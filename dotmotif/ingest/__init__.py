ad7b08bf2e7b1c5e04950f72d3db1e96df52bc7c
Jordan Matelsky <j6k4m8@gmail.com>
2020-08-03 20:44:47 -0400

    Add updated nx ingester

--- dotmotif/ingest/__init__.py
+++ dotmotif/ingest/__init__.py
@@ -20,6 +20,7 @@ class NetworkXConverter(abc.ABC):
     An abstract base class for import to the NetworkX format.
 
     """
+
     pass
 
     def to_graph(self) -> nx.Graph:
@@ -32,14 +33,15 @@ class CSVEdgelistConverter(NetworkXConverter):
 
     """
 
-    def __init__(self, filepath: str, u_id_column: str, v_id_column: str, directed: bool=True):
-        data = pd.read_csv(filepath, dtype={
-            u_id_column: str,
-            v_id_column: str,
-        })
+    def __init__(
+        self, filepath: str, u_id_column: str, v_id_column: str, directed: bool = True
+    ):
+        data = pd.read_csv(filepath, dtype={u_id_column: str, v_id_column: str,})
         self._graph = nx.DiGraph() if directed else nx.Graph()
         for i, row in data.iterrows():
-            self._graph.add_edge(str(row[u_id_column]), str(row[v_id_column]), **dict(row))
+            self._graph.add_edge(
+                str(row[u_id_column]), str(row[v_id_column]), **dict(row)
+            )
 
     def to_graph(self):
         return self._graph
@@ -78,7 +80,99 @@ class FileIngester(Ingester):
             os.makedirs(self.export_dir)
 
 
-class NetworkXIngester(Ingester):
+class NetworkXIngester:
+    """
+    An ingester that converts an in-memory networkx graph into two files:
+
+    One file contains the node IDs and their attrs, and the other contains
+    the edges and the edge attrs. Each column must contain a type annotation
+    in order for Neo4j to be able to interpret the attributes as variables.
+    """
+
+    def __init__(self, graph: nx.Graph, export_dir: str) -> None:
+        """
+        Create a new ingester from networkx format.
+
+        Arguments:
+            graph (nx.Graph): The in-memory graph to ingest
+            export_dir (str): The path to the CSV directory to use
+
+        """
+        self.export_dir = export_dir.rstrip("/") + "/"
+        self.graph = graph
+
+    def ingest(self) -> List[str]:
+        def _pd_cypher_dtype_map(dtype):
+            _valid_cypher_dtypes = [
+                # https://neo4j.com/docs/operations-manual/current/tools/import/file-header-format/
+                "int",
+                "long",
+                "float",
+                "double",
+                "boolean",
+                "byte",
+                "short",
+                # "char", "string",
+                # "point", "date", "localtime", "time", "localdatetime", "datetime", "duration"
+            ]
+            # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.api.types.infer_dtype.html
+            return {
+                "object": "string",
+                "string": "string",
+                "bytes": "string",
+                "mixed-integer-float": "float",
+                "floating": "float",
+                "integer": "int",
+                "mixed-integer": "int",
+            }.get(pd.api.types.infer_dtype(dtype, skipna=True), "string")
+
+        # First make sure that none of the IDs are numbers:
+        for n in self.graph.nodes():
+            if isinstance(n, (int, float)):
+                raise ValueError(
+                    "DotMotif does not support haystack graphs with numerical IDs. "
+                    + "Not all executors can operate on numerical-ID graphs, and so "
+                    + "they are disallowed. "
+                    + "Learn more at https://github.com/aplbrain/dotmotif/issues/26"
+                )
+
+        # Now create a node-attribute dataframe:
+        nodes_df = pd.DataFrame(
+            {"neuronId:ID(Neuron)": u, **attrs}
+            for u, attrs in self.graph.nodes(data=True)
+        )
+
+        # And edges:
+        edges_df = pd.DataFrame(
+            {":START_ID(Neuron)": u, ":END_ID(Neuron)": v, **attrs}
+            for u, v, attrs in self.graph.edges(data=True)
+        )
+
+        edges_df.columns = [
+            f"{c}"
+            + (f":{_pd_cypher_dtype_map(edges_df[c])}" if "(Neuron)" not in c else "")
+            for c in edges_df.columns
+        ]
+
+        nodes_df.columns = [
+            f"{c}"
+            + (f":{_pd_cypher_dtype_map(nodes_df[c])}" if "(Neuron)" not in c else "")
+            for c in nodes_df.columns
+        ]
+
+        # Export the files:
+        os.makedirs(f"{self.export_dir}", exist_ok=True)
+
+        nodes_df.to_csv(f"{self.export_dir}/export-neurons-0.csv")
+        edges_df.to_csv(f"{self.export_dir}/export-synapses-zdata.csv")
+
+        return [
+            f"{self.export_dir}/export-neurons-0.csv",
+            f"{self.export_dir}/export-synapses-zdata.csv",
+        ]
+
+
+class _deprecated_NetworkXIngester(Ingester):
     """
     An ingester from the NetworkX in-memory format.
 
