a946e099c2682e9cfd7972886be1879f84e419fc
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2020-08-07 23:27:44 -0400

    Add updated nx ingester (#79)
    * Add updated nx ingester
    
    * Remove deprecated nx ingest
    
    * Change edge names
    
    * fix edge name lookup
    
    * Update changelog
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-27-215.ec2.internal>

--- dotmotif/ingest/__init__.py
+++ dotmotif/ingest/__init__.py
@@ -20,6 +20,7 @@ class NetworkXConverter(abc.ABC):
     An abstract base class for import to the NetworkX format.
 
     """
+
     pass
 
     def to_graph(self) -> nx.Graph:
@@ -32,14 +33,15 @@ class CSVEdgelistConverter(NetworkXConverter):
 
     """
 
-    def __init__(self, filepath: str, u_id_column: str, v_id_column: str, directed: bool=True):
-        data = pd.read_csv(filepath, dtype={
-            u_id_column: str,
-            v_id_column: str,
-        })
+    def __init__(
+        self, filepath: str, u_id_column: str, v_id_column: str, directed: bool = True
+    ):
+        data = pd.read_csv(filepath, dtype={u_id_column: str, v_id_column: str,})
         self._graph = nx.DiGraph() if directed else nx.Graph()
         for i, row in data.iterrows():
-            self._graph.add_edge(str(row[u_id_column]), str(row[v_id_column]), **dict(row))
+            self._graph.add_edge(
+                str(row[u_id_column]), str(row[v_id_column]), **dict(row)
+            )
 
     def to_graph(self):
         return self._graph
@@ -78,11 +80,13 @@ class FileIngester(Ingester):
             os.makedirs(self.export_dir)
 
 
-class NetworkXIngester(Ingester):
+class NetworkXIngester:
     """
-    An ingester from the NetworkX in-memory format.
+    An ingester that converts an in-memory networkx graph into two files:
 
-    .
+    One file contains the node IDs and their attrs, and the other contains
+    the edges and the edge attrs. Each column must contain a type annotation
+    in order for Neo4j to be able to interpret the attributes as variables.
     """
 
     def __init__(self, graph: nx.Graph, export_dir: str) -> None:
@@ -98,106 +102,69 @@ class NetworkXIngester(Ingester):
         self.graph = graph
 
     def ingest(self) -> List[str]:
-        """
-        Perform the ingest into a list of CSV filenames.
+        def _pd_cypher_dtype_map(dtype):
+            _valid_cypher_dtypes = [
+                # https://neo4j.com/docs/operations-manual/current/tools/import/file-header-format/
+                "int",
+                "long",
+                "float",
+                "double",
+                "boolean",
+                "byte",
+                "short",
+                # "char", "string",
+                # "point", "date", "localtime", "time", "localdatetime", "datetime", "duration"
+            ]
+            # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.api.types.infer_dtype.html
+            return {
+                "object": "string",
+                "string": "string",
+                "bytes": "string",
+                "mixed-integer-float": "float",
+                "floating": "float",
+                "integer": "int",
+                "mixed-integer": "int",
+            }.get(pd.api.types.infer_dtype(dtype, skipna=True), "string")
 
-        .
-        """
         # First make sure that none of the IDs are numbers:
         for n in self.graph.nodes():
             if isinstance(n, (int, float)):
                 raise ValueError(
-                    "DotMotif does not support haystack graphs with numerical IDs. Not all executors can operate on numerical-ID graphs, and so they are disallowed. Learn more at https://github.com/aplbrain/dotmotif/issues/26"
+                    "DotMotif does not support haystack graphs with numerical IDs. "
+                    + "Not all executors can operate on numerical-ID graphs, and so "
+                    + "they are disallowed. "
+                    + "Learn more at https://github.com/aplbrain/dotmotif/issues/26"
                 )
 
-        # Export the graph to CSV (nodes and edges):
-        all_node_attrs = {}
-        for _, a in self.graph.nodes(data=True):
-            for key, val in a.items():
-                val = utils.untype_string(val)
-                if key in all_node_attrs:
-                    if isinstance(val, all_node_attrs[key]):
-                        pass
-                    else:
-                        all_node_attrs[key] = str
-                else:
-                    if isinstance(val, (float, int)):
-                        all_node_attrs[key] = float
-                    else:
-                        all_node_attrs[key] = str
-
-        for k, v in all_node_attrs.items():
-            all_node_attrs[k] = {str: "string", float: "float", int: "int"}[v]
-
-        sorted_node_attrs = sorted(list(all_node_attrs.keys()))
-
-        nodes_csv = (
-            "neuronId:ID(Neuron),"
-            + ",".join(
-                ["{}:{}".format(k, all_node_attrs[k]) for k in sorted_node_attrs]
-            )
-            + "\n"
-            + "\n".join(
-                [
-                    (i + "," + ",".join([str(n.get(k, "")) for k in sorted_node_attrs]))
-                    for i, n in self.graph.nodes(True)
-                ]
-            )
+        # Now create a node-attribute dataframe:
+        nodes_df = pd.DataFrame(
+            {"neuronId:ID(Neuron)": u, **attrs}
+            for u, attrs in self.graph.nodes(data=True)
         )
 
-        # This huge mess is type inference to make the exported file convey the
-        # types of the attributes, if available. String is the default, and is
-        # used if multiple datatypes are detected (for example, str and int
-        # defaults to String. int and float default to float.)
-        # TODO: Can probably be cleaned up!
-        all_edge_attrs = {}
-        for _, _, a in self.graph.edges(data=True):
-            for key, val in a.items():
-                val = utils.untype_string(val)
-                if key in all_edge_attrs:
-                    if isinstance(val, all_edge_attrs[key]):
-                        pass
-                    else:
-                        # Set the "expected type" the first time you see the
-                        # attribute in the entries
-                        all_edge_attrs[key] = str
-                else:
-                    if isinstance(val, (float, int)):
-                        # Set the dtype to float (which includes all ints)
-                        all_edge_attrs[key] = float
-                    else:
-                        # Set the dtype to string; we couldn't be more specific
-                        all_edge_attrs[key] = str
-
-        for k, v in all_edge_attrs.items():
-            all_edge_attrs[k] = {str: "string", float: "float", int: "int"}[v]
-
-        sorted_edge_attrs = sorted(list(all_edge_attrs.keys()))
-        edges_csv = (
-            ":START_ID(Neuron),:END_ID(Neuron),"
-            + ",".join(
-                ["{}:{}".format(k, all_edge_attrs[k]) for k in sorted_edge_attrs]
-            )
-            + "\n"
-            + "\n".join(
-                [
-                    f"{u},{v},"
-                    + ",".join([str(a.get(k, "")) for k in sorted_edge_attrs])
-                    for u, v, a in self.graph.edges(data=True)
-                ]
-            )
+        # And edges:
+        edges_df = pd.DataFrame(
+            {":START_ID(Neuron)": u, ":END_ID(Neuron)": v, **attrs}
+            for u, v, attrs in self.graph.edges(data=True)
         )
+
+        edges_df.columns = [
+            f"{c}"
+            + (f":{_pd_cypher_dtype_map(edges_df[c])}" if "(Neuron)" not in c else "")
+            for c in edges_df.columns
+        ]
+
+        nodes_df.columns = [
+            f"{c}"
+            + (f":{_pd_cypher_dtype_map(nodes_df[c])}" if "(Neuron)" not in c else "")
+            for c in nodes_df.columns
+        ]
 
         # Export the files:
-        try:
-            os.makedirs(f"{self.export_dir}")
-        except:
-            pass
+        os.makedirs(f"{self.export_dir}", exist_ok=True)
 
-        with open(f"{self.export_dir}/export-neurons-0.csv", "w") as fh:
-            fh.write(nodes_csv)
-        with open(f"{self.export_dir}/export-synapses-zdata.csv", "w") as fh:
-            fh.write(edges_csv)
+        nodes_df.to_csv(f"{self.export_dir}/export-neurons-0.csv")
+        edges_df.to_csv(f"{self.export_dir}/export-synapses-zdata.csv")
 
         return [
             f"{self.export_dir}/export-neurons-0.csv",
