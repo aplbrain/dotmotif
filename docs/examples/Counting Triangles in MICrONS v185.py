38b4998212caf73b6f1849d9505776ee1aa8262e
Jordan Matelsky <j6k4m8@gmail.com>
2020-10-14 10:18:20 -0400

    Update documentation and examples to use new API

--- docs/examples/Counting Triangles in MICrONS v185.py
+++ docs/examples/Counting Triangles in MICrONS v185.py
@@ -1,19 +1,22 @@
-from dotmotif import dotmotif, GrandIsoExecutor
+from dotmotif import Motif, GrandIsoExecutor
 from dotmotif.ingest import CSVEdgelistConverter
 
 
 graph = CSVEdgelistConverter(
     "https://zenodo.org/record/3710459/files/soma_subgraph_synapses_spines_v185.csv?download=1",
-    "pre_root_id", "post_root_id"
+    "pre_root_id",
+    "post_root_id",
 ).to_graph()
 
 E = GrandIsoExecutor(graph=graph)
 
-motif = dotmotif().from_motif("""
+motif = Motif(
+    """
 A -> B
 B -> C
 C -> A
-""")
+"""
+)
 
 results = E.find(motif)
 
