d94d6dd042b625617b7d5ebd01c3ecd806f4662d
Jordan Matelsky <j6k4m8@gmail.com>
2021-05-28 16:55:00 -0400

    Add grandiso implementation that uses the structural match

--- dotmotif/executors/GrandIsoExecutor.py
+++ dotmotif/executors/GrandIsoExecutor.py
@@ -17,10 +17,10 @@ limitations under the License.`
 import networkx as nx
 from grandiso import find_motifs_iter
 
-from .NetworkXExecutor import NetworkXExecutor
+from .NetworkXExecutor import NetworkXExecutor, _node_satisfies_constraints
 
 
-class GrandIsoExecutor(NetworkXExecutor):
+class GrandIsoBruteForceExecutor(NetworkXExecutor):
     def find(self, motif, limit: int = None):
         """
         Find a motif in a larger graph.
@@ -85,6 +85,83 @@ class GrandIsoExecutor(NetworkXExecutor):
                 and self._validate_node_constraints(
                     r, self.graph, motif.list_node_constraints()
                 )
+                and self._validate_dynamic_node_constraints(
+                    r, self.graph, motif.list_dynamic_node_constraints()
+                )
+                # by default, networkx returns the automorphism that is left-
+                # sorted, so this comparison is _opposite_ the check that we
+                # use in the other executors. In other words, we usually check
+                # that A >= B; here we check A <= B.
+                and (
+                    (not motif.exclude_automorphisms)
+                    or all(r[a] <= r[b] for (a, b) in motif.list_automorphisms())
+                )
+            ):
+                results.append(r)
+                if limit and len(results) >= limit:
+                    return results
+
+        return results
+
+
+class GrandIsoExecutor(NetworkXExecutor):
+    def find(self, motif, limit: int = None):
+        """
+        Find a motif in a larger graph.
+
+        Arguments:
+            motif (dotmotif.dotmotif)
+
+        """
+        # We need to first remove "negative" nodes from the motif, and then
+        # filter them out later on. Though this reduces the speed of the graph-
+        # matching, NetworkX does not seem to support this out of the box.
+
+        if motif.ignore_direction or not self.graph.is_directed:
+            graph_constructor = nx.Graph
+        else:
+            graph_constructor = nx.DiGraph
+
+        only_positive_edges_motif = graph_constructor()
+        must_not_exist_edges = []
+        for u, v, attrs in motif.to_nx().edges(data=True):
+            if attrs["exists"] is True:
+                only_positive_edges_motif.add_edge(u, v, **attrs)
+            elif attrs["exists"] is False:
+                # Collect a list of neg-edges to check for again in a moment
+                must_not_exist_edges.append((u, v))
+
+        def _doesnt_have_any_of_motifs_negative_edges(mapping):
+            for u, v in must_not_exist_edges:
+                if self.graph.has_edge(mapping[u], mapping[v]):
+                    return False
+            return True
+
+        constraints = motif.list_node_constraints()
+
+        def _node_attr_match_fn(
+            motif_node_id: str, host_node_id: str, motif_nx: nx.Graph, host_nx: nx.Graph
+        ):
+            return _node_satisfies_constraints(
+                host_nx.nodes[host_node_id], constraints.get(motif_node_id, {})
+            )
+            return True
+
+        graph_matches = find_motifs_iter(
+            only_positive_edges_motif,
+            self.graph,
+            is_node_attr_match=_node_attr_match_fn,
+        )
+
+        results = []
+        for r in graph_matches:
+            if _doesnt_have_any_of_motifs_negative_edges(r) and (
+                self._validate_edge_constraints(
+                    r, self.graph, motif.list_edge_constraints()
+                )
+                # and self._validate_node_constraints(
+                #     r, self.graph, motif.list_node_constraints()
+                # )
                 and self._validate_dynamic_node_constraints(
                     r, self.graph, motif.list_dynamic_node_constraints()
                 )
