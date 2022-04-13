ee81702a8cb994a826cfacdf0000b104f0c5ca7a
Jordan Matelsky <j6k4m8@gmail.com>
2022-04-12 22:31:31 -0400

    Add quote escaping tests

--- dotmotif/executors/test_neo4jexecutor.py
+++ dotmotif/executors/test_neo4jexecutor.py
@@ -1,5 +1,5 @@
 import dotmotif
-from dotmotif.executors.Neo4jExecutor import Neo4jExecutor
+from dotmotif.executors.Neo4jExecutor import Neo4jExecutor, _quoted_if_necessary
 import unittest
 import networkx as nx
 
@@ -31,3 +31,14 @@ class TestNeo4jExecutor_Automorphisms(unittest.TestCase):
         dm = dotmotif.Motif(exp, exclude_automorphisms=True)
         cypher = Neo4jExecutor.motif_to_cypher(dm)
         self.assertIn("id(A) < id(B)", cypher)
+
+
+class TestQuotingIfNecessary(unittest.TestCase):
+    def test_quoting_if_necessary(self):
+        self.assertEqual(_quoted_if_necessary('"xyz"'), '"xyz"'),
+        self.assertEqual(_quoted_if_necessary("'xyz'"), "'xyz'"),
+        self.assertEqual(_quoted_if_necessary("""x'y"z"""), '''"x'y\\\"z"'''),
+        self.assertEqual(_quoted_if_necessary('foo "bar"'), "'foo \"bar\"'"),
+        self.assertEqual(_quoted_if_necessary("""don't break"""), '''"don't break"'''),
+        self.assertEqual(_quoted_if_necessary("foo bar"), '"foo bar"')
+        self.assertEqual(_quoted_if_necessary("foo"), '"foo"')
