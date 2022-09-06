33a2f7011e97ae533b94bbae523118e8fe44969a
Jakob Troidl <jakob.troidl@googlemail.com>
2022-09-06 10:55:19 -0400

    new Neuprint edge constraints test

--- dotmotif/executors/test_neuprintexecutor.py
+++ dotmotif/executors/test_neuprintexecutor.py
@@ -52,12 +52,22 @@ if TOKEN:
             print(E.motif_to_cypher(motif))
             self.assertEqual(E.count(motif, limit=50), 35)
 
-        def test_edge_constraints(self):
+        def test_edge_constraints_notation1(self):
             motif = Motif().from_motif(
                 """
                 A -> B as AB
-                AB['CRE(L)'] > 10
-                AB['CX'] > 20
+                AB["CRE(L)"] > 10
+                AB["CX"] > 20
+                """
+            )
+            E = NeuPrintExecutor(HOST, DATASET, TOKEN)
+            print(E.motif_to_cypher(motif))
+            self.assertTrue(len(E.find(motif=motif, limit=5)) == 5)
+
+        def test_edge_constraints_notation2(self):
+            motif = Motif().from_motif(
+                """
+                A -> B ["CRE(L)" > 10, "CX" > 20]
                 """
             )
             E = NeuPrintExecutor(HOST, DATASET, TOKEN)
